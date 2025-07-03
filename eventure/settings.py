d0d4e118949f657b586e47155eefc9960deeb0c2
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2025-07-04 00:19:47 +0600

    Database setup

--- eventure/settings.py
+++ eventure/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,15 +89,24 @@ WSGI_APPLICATION = 'eventure.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'eventure',
+#         'USER': 'postgres',
+#         'PASSWORD': '79726858',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'eventure',
-        'USER': 'postgres',
-        'PASSWORD': '79726858',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://eventure_db_zved_user:CE1ggQ4oiQPx4ehVlP0wlOl2k8QIcJUL@dpg-d1jce27fte5s73eq1010-a.oregon-postgres.render.com/eventure_db_zved',
+        conn_max_age=600
+    )
 }
 
 
